2f0245f5ea0844f0cdfd34f861395ad51a395f91
kumagai <kumagai@macnoMacBook-Pro.local>
2024-11-21 22:31:00 +0900

    三連複分析を追加

--- stat_tansho.py
+++ stat_tansho.py
@@ -3,9 +3,16 @@ import sys
 import keiba_lib
 
 ninki_limit = None
+youbi = None
+race_filter = None
+histo = False
 for arg in sys.argv:
     if arg.startswith('-ninki_limit='):
-        ninki_limit = int(arg.split('=')[1])
+        ninki_limit = int(arg[arg.index('=')+1:])
+    elif arg.startswith('-youbi='):
+        youbi = arg[arg.index('=')+1:]
+    elif arg.startswith('-race_filter='):
+        race_filter = arg[arg.index('=')+1:]
 
 with open('race_result.json') as race_json_file:
     race_json = json.load(race_json_file)
@@ -13,6 +20,9 @@ with open('race_result.json') as race_json_file:
     total_total_win_yen_sum = []
     ninki_histo = {i: 0 for i in range(1, 19)}
     for day in race_json:
+        if youbi is not None and day[11] != youbi:
+            continue
+
         for location in race_json[day]:
             top_ninki_list = []
             top_win_yen_sum = []
@@ -21,6 +31,13 @@ with open('race_result.json') as race_json_file:
             total_horse_cnt = 0
             for race_no in race_json[day][location]:
                 result = race_json[day][location][race_no]
+
+                if race_filter is not None:
+                    if race_filter == 'heavy' and len(race_no) < 2:
+                        continue
+                    elif race_filter.startswith('horse_cnt_') and len(result['rank_list']) > int(race_filter[10:]):
+                        continue
+
                 total_horse_cnt += len(result['rank_list'])
                 (rank, horse_no, horse_name, jocky, ninki) = result['rank_list'][0]
                 if ninki == 1:
@@ -31,13 +48,13 @@ with open('race_result.json') as race_json_file:
                 top_ninki_list.append(ninki)
                 ninki_histo[ninki] += 1
 
-                if race_no in ('10', '11', '12'):
-                    total_bet += 100 * ninki_limit
-                    if ninki_limit is None or ninki <= ninki_limit:
-                        total_total_win_yen_sum.append(result['win_yen'])
+                total_bet += 100 * ninki_limit
+                if ninki_limit is None or ninki <= ninki_limit:
+                    total_total_win_yen_sum.append(result['win_yen'])
             print(f"{day} {location} トップの人気：{' '.join([str(n) for n in top_ninki_list])} トップの配当：{top_win_yen_sum} 最高配当：{max_win_yen} 全掛け金：{100 * total_horse_cnt} 全配当：{total_win_yen_sum}")
 
 print(f'賭け金：{total_bet:,}円 配当：{sum(total_total_win_yen_sum):,}円')
 
-for i, count in ninki_histo.items():
-    print(f"{i:2} {'*' * count}")
+if histo:
+    for i, count in ninki_histo.items():
+        print(f"{i:2} {'*' * count}")
---
+++ stat_trio.py
@@ -0,0 +1,51 @@
+import json
+import sys
+import keiba_lib
+
+trio_no = None
+youbi = None
+race_filter = None
+for arg in sys.argv:
+    if arg.startswith('-trio_no='):
+        trio_no = [int(n) for n in arg[arg.index('=')+1:].split(',')]
+    elif arg.startswith('-youbi='):
+        youbi = arg[arg.index('=')+1:]
+    elif arg.startswith('-race_filter='):
+        race_filter = arg[arg.index('=')+1:]
+
+if trio_no is None:
+    trio_no = [1, 2, 3]
+
+with open('race_result.json') as race_json_file:
+    race_json = json.load(race_json_file)
+    trio_yen_sum = []
+    total_bet = 0
+    for day in race_json:
+        if youbi is not None and day[11] != youbi:
+            continue
+
+        for location in race_json[day]:
+            subtrio_yen_sum = []
+            subtotal_bet = 0
+            for race_no in race_json[day][location]:
+                result = race_json[day][location][race_no]
+
+                if race_filter is not None:
+                    if race_filter == 'heavy' and len(race_no) < 2:
+                        continue
+                    elif race_filter.startswith('horse_cnt_') and len(result['rank_list']) > int(race_filter[10:]):
+                        continue
+
+                (rank1, horse_no1, horse_name1, jocky1, ninki1) = result['rank_list'][0]
+                (rank2, horse_no2, horse_name2, jocky2, ninki2) = result['rank_list'][1]
+                (rank3, horse_no3, horse_name3, jocky3, ninki3) = result['rank_list'][2]
+                ninki_list = (ninki1, ninki2, ninki3)
+                if trio_no[0] in ninki_list and trio_no[1] in ninki_list and trio_no[2] in ninki_list:
+                    trio_yen_sum.append(result['trio_yen'])
+                    subtrio_yen_sum.append(result['trio_yen'])
+                subtotal_bet += 100
+                total_bet += 100
+            print(f"{day} {location} {subtotal_bet} {subtrio_yen_sum}={sum(subtrio_yen_sum):,}")
+
+print()
+print(f"賭け金：{total_bet:,}円 配当金：{sum(trio_yen_sum):,}円")
